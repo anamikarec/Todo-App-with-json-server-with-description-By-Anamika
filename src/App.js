40f41d8a5a18149105b7e90063e1ed4a175d9c81
Anamika Gupta <65801101+anamikarec@users.noreply.github.com>
2022-01-18 11:04:42 +0530

    localStorage is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./styles.css";
 import Todo from "./Todo/Todo";
 import Login from "./Login/Login";
-import { useSelector } from "react-redux";
+import { shallowEqual, useSelector } from "react-redux";
 export default function App() {
+  // const isAuth2 = useSelector(
+  //   (state) => ({ isAuth: state.isAuth, token: state.token }),
+  //   shallowEqual
+  // );
+  // or
   const isAuth = useSelector((state) => state.isAuth);
+  const token = useSelector((state) => state.token);
+
   return isAuth ? (
     <div className="App">
+      <h3>User Token : {token}</h3>
       <h1>Todo</h1>
       <Todo />
     </div>
@@ -23,3 +31,6 @@ export default function App() {
 // Working with multiple reducers/combined reducers
 // working with dev tools
 // midddlewares
+
+// this is not the recommended because this will re-render the app every single time.
+// const {isAuth,token} = useSelector((state)=>state);
--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,8 +1,19 @@
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../redux/action";
+
 function Login() {
+  const dispatch = useDispatch();
+
+  const handleAdd = () => {
+    const action = loginSuccess(Date.now());
+    dispatch(action);
+    // or
+    // dispatch(loginSuccess(Date.now()))
+  };
   return (
     <div>
       <h3>Login</h3>
-      <button>Please click on this to login</button>
+      <button onClick={handleAdd}>Please click on this to login</button>
     </div>
   );
 }
